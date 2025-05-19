6d211604c09bc1ba699922de8f58bbb3a22712d6
Hiba Daoud <hibadaoud69@gmail.com>
2025-05-19 16:16:17 +0200

    delete comments

--- spark/spark_udfs.py
+++ spark/spark_udfs.py
@@ -1,17 +1,4 @@
 
-# iata_country_dict = {}
-# iata_position_dict = {}
-# iata_name_dict = {}
-
-# def set_iata_dicts(country_dict, position_dict, name_dict):
-#     global iata_country_dict, iata_position_dict, iata_name_dict
-#     iata_country_dict = country_dict
-#     iata_position_dict = position_dict
-#     iata_name_dict = name_dict
-
-# Define the flight type determination function
-# spark/spark_udfs.py
-
 def make_determine_flight_type(b_country_dict):
     def _determine_flight_type(dep_iata, arr_iata):
         dep_country_code = b_country_dict.value.get(dep_iata, None)
